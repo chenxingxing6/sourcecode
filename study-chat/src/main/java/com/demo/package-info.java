b8c448d3b39f421c7b952e89a31af9d9ae839ef7
lanxinghua <lanxinghua@2dfire.com>
2019-10-10 15:23:11 +0800

    chat

--- study-chat/src/main/java/com/demo/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @Author: cxx
- * @Date: 2019/10/6 11:24
- */
-package com.demo;
---
+++ study-chat/src/main/java/com/demo/server/ChatServer.java
@@ -0,0 +1,18 @@
+package com.demo.server;
+
+/**
+ * User: lanxinghua
+ * Date: 2019/10/10 14:58
+ * Desc: 聊天服务端
+ */
+public class ChatServer {
+    public static void main(String[] args) {
+        new ChatServer().start();
+
+    }
+
+    public void start(){
+        System.out.println();
+
+    }
+}
